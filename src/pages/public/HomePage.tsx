5d1c3164b8a81c46084aea3a297b6001c611ffff
Jeanny Fidelica <jannyfidelica@gmail.com>
2023-03-09 21:56:58 +0300

    first commit

---
+++ src/pages/public/HomePage.tsx
@@ -0,0 +1,13 @@
+import clsx from "clsx";
+
+
+
+function HomePage() {
+	 return (<div className={clsx(
+		'w-full h-full bg-blue-500'
+	 )}>
+		  HomePage
+	 </div>);
+}
+
+export default HomePage;
